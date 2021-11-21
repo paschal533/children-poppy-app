ddb928cdb552d013ff8675287e52db2963ff9be9
unknown <okwuosahpashal@gmail.com>
2021-11-21 02:32:51 +0100

    forth commit

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { submitComment } from '../services';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
