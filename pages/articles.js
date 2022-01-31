ca938f69d27a0ce13c8360cabf169bd71b4e5eea
unknown <okwuosahpaschal@gmail.com>
2022-01-31 21:38:34 +0100

    article commit

---
+++ pages/articles.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { PostCard, Categories, PostWidget, Loader } from '../components';
+import { getDisplayPosts } from '../services';
+
+export default function Articles() {
+  const [postsResult, setPostsResult] = useState([]);
+
+  useEffect(() => {
+    getDisplayPosts().then((result) => {
+      setPostsResult(result);
+    });
+  }, []);
+
+  return (
+    <div className="container mx-auto px-2 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {postsResult?.length !== 0 ? postsResult?.reverse().slice(4).map((post, index) => (
+            <PostCard key={index} post={post} />
+          )) : <Loader />}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget, Loader } from '../components';
 import { getDisplayPosts } from '../services';
@@ -17,9 +18,16 @@ export default function Home() {
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {postsResult?.length !== 0 ? postsResult?.reverse().map((post, index) => (
-            <PostCard key={index} post={post} />
+          {postsResult?.length !== 0 ? postsResult?.reverse().slice(0, 3).map((post, index) => (
+            <div>
+              <PostCard key={index} post={post} />
+            </div>
           )) : <Loader />}
+        </div>
+        <div className="text-center">
+          <Link href="/articles">
+            <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">View more articles</span>
+          </Link>
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
