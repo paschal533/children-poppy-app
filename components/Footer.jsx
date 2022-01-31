a4e990c618eadb76e21019318d27225c065bd812
unknown <okwuosahpaschal@gmail.com>
2022-01-31 23:41:45 +0100

    footer commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -31,13 +31,13 @@ const Footer = () => (
     </div>
 
     <div className="flex justify-center items-center flex-col mt-5">
-      <p className="text-white text-sm text-center">Come join us and hear for the unexpected miracle</p>
-      <p className="text-white text-sm text-center font-medium mt-2">info@kryptomastery.com</p>
+      <p className="text-white text-sm text-center">Come join us and learn more about children</p>
+      <p className="text-white text-sm text-center font-medium mt-2">childrenspoppy@gmail.com</p>
     </div>
 
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
-    <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+    <div className="sm:w-[90%] w-70% w-full flex justify-between items-center mt-3">
       <p className="text-white text-left text-xs">@Chilren's poppy 2022</p>
       <p className="text-white text-right text-xs">All rights reserved</p>
     </div>
