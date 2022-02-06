e9272b5c88b1104b79f2206cdc0f0c6307b01de3
unknown <okwuosahpaschal@gmail.com>
2022-02-06 22:25:39 +0100

    fifth commit

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -33,7 +33,7 @@ const PostWidget = ({ categories, slug }) => {
               height="60px"
               width="60px"
               unoptimized
-              className="align-middle rounded-full"
+              //className="align-middle rounded-full"
               src={post.featuredImage.url}
             />
           </div>
