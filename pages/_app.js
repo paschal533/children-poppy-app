2f8c32dbbe1ba0af566d27f661f932df60329735
unknown <okwuosahpaschal@gmail.com>
2022-04-02 22:15:40 +0100

    script commit

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,14 @@ import { Layout } from '../components';
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-2242621108621804"
+        async="true"
+        strategy="beforeInteractive"
+        onError={ (e) => { console.error('Script failed to load', e) }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
       <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js" />
       <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js" />
        <Component {...pageProps} />
