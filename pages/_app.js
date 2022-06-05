cb90d74785343c0a0cc17ea85247395f73f80bc0
paschal533 <okwuosahpaschal@gmail>
2022-06-05 21:08:32 +0100

    update commit

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,36 @@ import { Layout } from '../components';
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <NextSeo
+        title="Children's Poppy | A blog website for parents, teachers, children, caregivers, and all who have had a childhood experience"
+        description="Read our weekly updates about children's development and how to help them grow properly, be aware of how your childhood experiences affect your lifestyle"
+        defaultTitle="Children's Poppy | A blog website for parents, teachers, children, caregivers, and all who have had a childhood experience"
+        canonical="https://www.childrenspoppy.com/"
+        openGraph={{
+          url: "https://www.childrenspoppy.com/",
+          title: "Children's Poppy | A blog website for parents, teachers, children, caregivers, and all who have had a childhood experience",
+          description:
+            "Read our weekly updates about children's development and how to help them grow properly, be aware of how your childhood experiences affect your lifestyle",
+          images: [
+              {
+                url: "https://i.ibb.co/TvBCbb2/image-removebg-preview.png",
+                width: 1280,
+                height: 720,
+                alt: "Children's Poppy | A blog website for parents, teachers, children, caregivers, and all who have had a childhood experience",
+              },
+            ],
+          }}
+          twitter={{
+            cardType: "summary_large_image",
+          }}
+          additionalMetaTags={[
+            {
+              property: "keywords",
+              content:
+                "children, kids, parents, movies, books, cartoons, caregiver, development, classroom, teachers, childhood, child's needs",
+            },
+          ]}
+      />
       <Script
         id="Adsense-id"
         data-ad-client="ca-pub-2242621108621804"
